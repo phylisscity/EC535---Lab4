2cbd185536bb79a4e32d5a0b2c78695a467f5805
TymoteuszPardej <tymoteusz.par@gmail.com>
2025-10-27 11:17:04 -0400

    work in progress

--- mytraffic.c
+++ mytraffic.c
@@ -6,7 +6,7 @@
 #include <linux/jiffies.h>      // For jiffies, msecs_to_jiffies
 #include <linux/fs.h>           // For register_chrdev, file_operations
 #include <linux/uaccess.h>      // For copy_to_user()
-
+#include <linux/gpio/consumer.h>
 MODULE_LICENSE("GPL");
 
 // GPIO pin numbers from the lab doc
@@ -16,6 +16,8 @@ MODULE_LICENSE("GPL");
 #define GPIO_BTN0   26
 
 #define MAJOR_NUM 61
+
+static struct gpio_desc *led_red, *led_ye, *led_gr, *button_1, *button_2;
 
 // need to track which mode we're in
 // 0=normal, 1=flash-red, 2=flash-yellow
@@ -34,6 +36,13 @@ static int counter = 0;
 // need to store IRQ number so we can free it later
 static int irq_num;
 
+//track whether LEDs are on or off
+//used to decide whether to turn off or on an Led when blinking
+static int red_state = 0;
+static int ye_state = 0;
+
+static int major;
+
 // handles the normal mode sequence
 // green stays on 3 cycles, then yellow 1 cycle, then red 2 cycles, repeat
 static void handle_normal_mode(void)
@@ -46,6 +55,16 @@ static void handle_normal_mode(void)
 static void handle_flash_red(void)
 {
     // TODO
+    if (red_state == 0)
+    {
+        gpiod_set_value(led_red, 0);
+        red_state = 1;
+    }
+    else
+    {
+        gpiod_set_value(led_red, 1);
+        red_state = 0;
+    }
 }
 
 // handles flashing yellow mode
@@ -53,6 +72,16 @@ static void handle_flash_red(void)
 static void handle_flash_yellow(void)
 {
     // TODO
+    if (ye_state == 0)
+    {
+        gpiod_set_value(led_ye, 0);
+        ye_state = 1;
+    }
+    else
+    {
+        gpiod_set_value(led_ye, 1);
+        ye_state = 0;
+    }
 }
 
 // timer callback - kernel calls this every cycle
@@ -75,6 +104,11 @@ static void timer_callback(struct timer_list *t)
 static irqreturn_t btn0_isr(int irq, void *dev_id)
 {
     // TODO
+    current_mode++;
+    state = 0;
+    counter = 0;
+    //Now I need to interrput
+
     return IRQ_HANDLED;
 }
 
@@ -119,6 +153,7 @@ static struct file_operations fops = {
 static int __init traffic_init(void)
 {
     int ret;
+    int status;
     
     // TODO
     // request GPIO pins using gpio_request for each pin
@@ -127,6 +162,84 @@ static int __init traffic_init(void)
     // request interrupt with request_irq
     // register char device with register_chrdev using MAJOR_NUM
     // setup timer with timer_setup and start it with mod_timer
+
+    major = register_chrdev(MAJOR_NUM, "mytraffic", 0644, NULL, &fops)
+    if(major < 0)
+    {
+        printk(KERN_ALERT "Registering device failed\n");
+        return major;
+    }
+
+    led_red = gpio_to_desc(67);
+    if(!led_red)
+    {
+        printk(KERN_ALERT "Error acessing pin 67\n")
+        return -ENODEV;
+    }
+    led_ye = gpio_to_desc(68);
+    if(!led_ye)
+    {
+        printk(KERN_ALERT "Error acessing pin 68\n");
+        return - ENODEV;
+    }
+    led_gr = gpio_to_desc(44);
+    if(!led_gr)
+    {
+        printk(KERN_ALERT "Error acessing pin 44\n");
+        return -ENODEV;
+    }
+    button_1 = gpio_to_desc(26);
+    if(!button_1)
+    {
+        printk(KERN_ALERT "Error acessing pin 26\n");
+        return -ENODEV;
+    }
+    button_2 = gpio_to_desc(46);
+    if(!button_2)
+    {
+        printk(KERN_ALERT "Error acessing pin 46\n");
+        return -ENODEV;
+    }
+
+    status = gpiod_direction_output(led_red);
+    if (status)
+    {
+        printk(KERN_ALERT "Error setting pin 67 to output\n");
+        return status;
+    }
+
+    status = gpiod_direction_output(led_ye);
+    if (status)
+    {
+        printk(KERN_ALERT "Error setting pin 68 to output\n");
+        return status;
+    }
+
+    status = gpiod_direction_output(led_gr);
+    if (status)
+    {
+        printk(KERN_ALERT "Error setting pin 44 to output");
+        return status;
+    }
+
+    status = gpiod_direction_input  (button_1)
+    if (status)
+    {
+        printk(KERN_ALERT "Error setting pin 26 to input\n");
+        return status;
+    }
+
+    status = gpiod_direction_input(button_2);
+    {
+        printk(KERN_ALERT "Error setting pin 46 to input\n");
+    }
+        
+    gpiod_set_value(led_red, 0);
+    gpiod_set_value(led_ye, 0);
+    gpiod_set_value(led_gr, 0);
+
+    red_state = 1;
+    ye_state = 1;
     
     printk(KERN_INFO "mytraffic: module loaded\n");
     return 0;
@@ -142,6 +255,19 @@ static void __exit traffic_exit(void)
     // free interrupt with free_irq using irq_num
     // free GPIOs with gpio_free for each pin
     // unregister char device with unregister_chrdev
+
+    delt_timer(traffic_timer);
+
+    gpiod_set_value(led_red, 0);
+    gpiod_set_value(led_ye, 0);
+    gpiod_set_value(led_gr, 0);
+
+    gpio_free(led_red);
+    gpio_free(led_ye);
+    gpio_free(led_gr);
+    gpio_free(button_1);
+    gpio_free(button_2);
+
     
     printk(KERN_INFO "mytraffic: module unloaded\n");
 }
